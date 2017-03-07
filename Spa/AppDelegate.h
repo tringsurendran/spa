9d3a0f8ce6c6097671724d671fe02f744178e8dd
Surendran Thiyagarajan <surendran.t@tringapps.com>
2017-03-07 11:53:12 -0800

    Initial Commit

---
+++ Spa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spa
+//
+//  Created by Surendran Thiyagarajan on 3/7/17.
+//  Copyright © 2017 suren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spa
+//
+//  Created by Surendran Thiyagarajan on 3/7/17.
+//  Copyright © 2017 suren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
